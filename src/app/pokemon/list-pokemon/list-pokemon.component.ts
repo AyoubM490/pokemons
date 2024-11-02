ca2f54834c5c53333b375b7b44008e934e49e20a
AyoubM490 <chahayb563@gmail.com>
2024-11-02 14:22:04 +0100

    add angular app without api handling

---
+++ src/app/pokemon/list-pokemon/list-pokemon.component.ts
@@ -0,0 +1,27 @@
+import { PokemonService } from './../pokemon.service';
+import { Component, OnInit } from '@angular/core';
+import { Pokemon } from '../pokemon';
+import { POKEMONS } from '../mock-pokemon-list';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-list-pokemon',
+  templateUrl: './list-pokemon.component.html',
+  styles: [],
+})
+export class ListPokemonComponent implements OnInit {
+  pokemonList: Pokemon[];
+
+  /**
+   *
+   */
+  constructor(private router: Router, private pokemonService: PokemonService) {}
+
+  ngOnInit(): void {
+    this.pokemonList = this.pokemonService.getPokemonList();
+  }
+
+  goToPokemon(pokemon: Pokemon) {
+    this.router.navigate(['/pokemon', pokemon.id]);
+  }
+}
